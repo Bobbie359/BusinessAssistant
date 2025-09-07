ddf308e1a2458f04222af3c290f90fa75f10070f
Борислав <borislavignatov4@gmail.com>
2025-09-07 14:46:13 +0300

    Initial Commit

---
+++ BusinessAssistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessAssistant
+//
+//  Created by Borislav Ignatov on 7.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BusinessAssistantTests/BusinessAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessAssistantTests.swift
+//  BusinessAssistantTests
+//
+//  Created by Borislav Ignatov on 7.09.25.
+//
+
+import Testing
+@testable import BusinessAssistant
+
+struct BusinessAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
